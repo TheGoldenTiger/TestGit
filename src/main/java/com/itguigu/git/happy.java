f0751dd759344088b6d9f5418cffd1931e38e47a
tiger_glo <ti_glo@ger.com>
2019-02-19 15:02:43 +0800

    test2

--- src/main/java/com/itguigu/git/happy.java
+++ src/main/java/com/itguigu/git/happy.java
@@ -4,5 +4,7 @@ public class happy {
     public static void main(String[] args) {
         //test
         System.out.println("right");
+        //left
+        System.out.println("left");
     }
 }
